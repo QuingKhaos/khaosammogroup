64cda986ffc3ff79985f815fa038771dd7ad44e2
Em Karisch <emii.khaos@gmail.com>
2026-07-21 18:44:22 +0200

    feat: add support for atomic artillery shells

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,4 @@
+require("__khaosammogroup__.prototypes.compat.atomic-artillery-shells-final-fixes")
 require("__khaosammogroup__.prototypes.compat.more-ammo-final-fixes")
 require("__khaosammogroup__.prototypes.compat.oc-ammo-casting-final-fixes")
 require("__khaosammogroup__.prototypes.compat.space-age-ammo-final-fixes")
