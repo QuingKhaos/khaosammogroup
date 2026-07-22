42805bbf148b0f97d6d0be5847e104d6702eccd6
Em Karisch <emii.khaos@gmail.com>
2026-07-22 07:22:18 +0200

    fix: changed settings of OCs ammo casting

--- prototypes/compat/uranium-artillery-shell-final-fixes.lua
+++ prototypes/compat/uranium-artillery-shell-final-fixes.lua
@@ -4,8 +4,10 @@ if mods["Uranium-Artillery-Shell"] then
     lib.update_subgroup("ammo", "uranium-artillery-shell", "ammo-artillery-shell")
     lib.update_subgroup("ammo", "uranium-artillery-shell-mk2", "ammo-artillery-shell")
 
-  if mods["OCs_ammo_casting"] then
+  if mods["OCs_ammo_casting"] and settings.startup["allow-casting-explosive-ammo"].value then
     lib.update_recipe_subgroup("oc-casting-uranium-artillery-shell", "ammo-artillery-shell")
-    lib.update_recipe_subgroup("oc-casting-uranium-artillery-shell-mk2", "ammo-artillery-shell")
+    if settings.startup["nuclear-ammo"].value then
+      lib.update_recipe_subgroup("oc-casting-uranium-artillery-shell-mk2", "ammo-artillery-shell")
+    end
   end
 end
