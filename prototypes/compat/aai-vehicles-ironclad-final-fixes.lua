307d2524c6fa3d07e8b8d15fec77b98eae7f67eb
Em Karisch <emii.khaos@gmail.com>
2026-07-22 16:26:11 +0200

    feat: support AAI vehicles: Ironclad

---
+++ prototypes/compat/aai-vehicles-ironclad-final-fixes.lua
@@ -0,0 +1,6 @@
+local lib = require("__khaosammogroup__.prototypes.lib")
+
+if mods["aai-vehicles-ironclad"] then
+    lib.update_subgroup("ammo", "mortar-bomb", "ammo-grenade")
+    lib.update_subgroup("ammo", "mortar-cluster-bomb", "ammo-grenade")
+end
