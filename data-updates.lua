37eed502b030f58e8fe8ad064d64c144366df6be
Em Karisch <emii.khaos@gmail.com>
2026-07-24 20:18:44 +0200

    fix: assign recipes to subgroups

--- data-updates.lua
+++ data-updates.lua
@@ -12,7 +12,7 @@ if not settings.startup["khaosammogroup-disable-new-subgroups"].value then
 end
 
 for name, ammo in pairs(data.raw["ammo"]) do
-  lib.update_item_subgroup("ammo", name, "ammo-" .. ammo.ammo_category)
+  lib.update_subgroup("ammo", name, "ammo-" .. ammo.ammo_category)
 end
 
 lib.update_subgroup("capsule", "grenade", "ammo-grenade")
@@ -25,7 +25,7 @@ lib.update_subgroup("capsule", "distractor-capsule", "ammo-capsule")
 lib.update_subgroup("capsule", "destroyer-capsule", "ammo-capsule")
 
 for name, _ in pairs(data.raw["land-mine"]) do
-  lib.update_item_subgroup("item", name, "ammo-landmine")
+  lib.update_subgroup("item", name, "ammo-landmine")
 end
 
 -- e.g. from AAI Vehicles: Ironclad
@@ -33,7 +33,7 @@ for _, name in pairs(khaoslib_item.find("ammo", function(item)
   --- @cast item data.AmmoItemPrototype
   return item.ammo_category == "mortar-bomb"
 end)) do
-  lib.update_item_subgroup("ammo", name, "ammo-grenade")
+  lib.update_subgroup("ammo", name, "ammo-grenade")
 end
 
 require("__khaosammogroup__.prototypes.compat.k2-k2so-updates")
